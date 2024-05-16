b43b3a24686b3b78c362dda25b11e9c200542cc2
hha <hoxiea@gmail.com>
2024-05-16 09:57:55 -0700

    Back to basics: text.astro contains a Tailwind header that I actually understand

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hhackerman.com",
+  integrations: [tailwind(), mdx()]
 });
