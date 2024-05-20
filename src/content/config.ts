6f21a7bad3fc73aadd1c7f1550974e4e53fd32ad
hha <hoxiea@gmail.com>
2024-05-20 10:40:11 -0700

    Add basic listing of projects, based on data collection

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// https://docs.astro.build/en/guides/content-collections/#defining-a-collection-schema
+
+import { z, defineCollection } from "astro:content";
+
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    summary: z.string(),
+    linktext: z.string(),
+  }),
+});
+
+// The key must match the name of the folder
+export const collections = {
+  projects: projectCollection,
+};
---
+++ src/content/projects/hhackerman.md
@@ -0,0 +1,6 @@
+---
+title: hhackerman.com (This Site)
+date: Summer 2024
+summary: In the summer of 2024, I decided to revamp my personal website. I wanted to make it more modern and accessible while learning more about web programming. I also wanted to make it easier to update and maintain. It's nothing too fancy, but I'm happy with the result!
+linktext: Read more about the site
+---
