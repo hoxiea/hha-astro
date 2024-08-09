0542fbd242b8527fb164b01718f4cb15bd1ffffb
hha <hoxiea@gmail.com>
2024-08-09 14:44:42 -0700

    Add table of contents, finish rough draft of content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkSlug from "remark-slug";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hhackerman.com",
   integrations: [tailwind(), mdx(), react()],
+  markdown: {
+    remarkPlugins: [
+      remarkSlug,
+      [remarkToc, { heading: "Table of Contents", maxDepth: 2 }],
+    ],
+    rehypePlugins: [],
+  },
 });
